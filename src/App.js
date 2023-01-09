8d92c1202f98825c5b6317866bf7703f870aeedf
Eduard <eduard.korytnyk@gmail.com>
2023-01-09 08:03:19 +0200

    add axios

--- src/App.js
+++ src/App.js
@@ -1,69 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Search from "./components/search/search";
 import NewsData from "./data";
 import axios from 'axios';
 import './App.css'
 
-function App() {
+async function fetchData() {
   const baseURL = "https://newsapi.org/v2/top-headlines?country=ua&apiKey=";
   const APIkey = 'ed6cf96f82da4c07b0f9ce0eccf6de3b'
+  try {
+    const response = await axios.get(`${baseURL}${APIkey}`);
+    return response.data.articles;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function App() {
   const [data, setData] = useState([]);
 
-//******************* XMLHttpRequest example ********************* */
+  useEffect(() => {
+    fetchData().then(data => setData(data));
+  }, []);
 
-React.useEffect(() => {
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", `${baseURL}${APIkey}`, true);
-
-    xhr.onload = function (e) {
-            if (this.readyState == 4 && this.status == 200) {
-            let json_obj = JSON.parse(xhr.responseText);
-            setData(json_obj.articles)
-            } 
-        else 
-            {
-            console.error(xhr.statusText);
-            }
-        
-    }
-    xhr.onerror = function(error)
-        {
-        console.error( error )
-        }
-    xhr.send();
-    }, []);
-console.log(data)
-
-//******************* fetch example ********************* */
-
-    // React.useEffect(() => {
-    //     const params = {
-    //         path:'/v2/top-headlines?',
-    //         country: 'ua',
-    //         apiKey: 'ed6cf96f82da4c07b0f9ce0eccf6de3b'
-    //     };
-    //     const options = {
-    //         method: 'GET',
-    //     };
-    //     fetch( `https://newsapi.org${params.path}country=${params.country}&apiKey=${params.apiKey}`, options)
-    //     .then( response => response.json() )
-    //     .then( response => {
-    //         setData(response.articles)
-    //     });
-    //   }, []);
-  
-
-//******************* axios example ********************* */
-
-  // React.useEffect(() => {
-  //   axios.get(`${baseURL}${APIkey}`).then((response) => {
-  //       setData(response.data.articles);
-  //   });
-  // }, []);
-if (!data) return null;
+  if (!data) return null;
   return (
     <div className="App">
-  <Search data={data}/>
+      <Search data={data}/>
     </div>
   );
 }
